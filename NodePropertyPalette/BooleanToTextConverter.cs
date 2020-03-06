53eb49eaaffb19efe251a8b7a4f1070669e54454
Martin Misol Monzo <mmisol@users.noreply.github.com>
2020-03-06 11:30:19 -0500

    Add built-in column to node grid
    The built-in column does a best-effort attempt to show whether a node
    is originally from Dynamo or not. The original idea was to provide
    information about which nodes are from the host application, but we
    don't have a reliable way to check that. We might want to find a more
    realiable way to identify them in the future.
    
    Also, the layout of the row details panel is adjusted to wrap content
    so that the checkboxes don't cause horizontal scrolling.

---
+++ NodePropertyPalette/BooleanToTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NodePropertyPalette
+{
+    /// <summary>
+    /// Convert a bool to a Yes/No string.
+    /// </summary>
+    public class BooleanToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && (bool)value ? "Yes" : "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
